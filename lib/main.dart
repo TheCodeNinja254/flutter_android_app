ce0411525e9ba99b76c47e12ba2b58b7568214d7
fredrick <fmmbugua@safaricom.co.ke>
2022-05-19 16:02:05 +0300

    hello dart and flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
 void main () {
-
+  // with parenthesis - class object
+  // without parenthesis - type
+  runApp(MyApp());
 }
 
 // inheritance using extends
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   // BuildContext  - object type provided by flutter
   // context - my variable override
   // Widget - return object of type Widget
